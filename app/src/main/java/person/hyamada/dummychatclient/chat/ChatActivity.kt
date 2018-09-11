44dc900b640650c1f39c2214bed41cded91274ad
山田浩輝 <hyamada@gntm-metal.local>
2018-09-12 06:37:26 +0900

    resolve dependencies

--- app/src/main/java/person/hyamada/dummychatclient/chat/ChatActivity.kt
+++ app/src/main/java/person/hyamada/dummychatclient/chat/ChatActivity.kt
@@ -3,7 +3,10 @@ package person.hyamada.dummychatclient.chat
 import android.content.Intent
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.support.v7.widget.LinearLayoutManager
+import android.support.v7.widget.RecyclerView
 import android.util.Log
+import android.view.View
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
@@ -13,25 +16,26 @@ import person.hyamada.dummychatclient.data.Message
 import java.io.IOException
 
 class ChatActivity : AppCompatActivity() {
-    lateinit var mPresenter: ChatPresenter
+
+    private var mPresenter: ChatPresenter? = null
+    private var mRecyclerView: RecyclerView? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_chat)
         mPresenter = ChatPresenter(this)
-        findViewById<TextView>(R.id.textView1).setText(mPresenter.client)
-        findViewById<TextView>(R.id.textView2).setText(mPresenter.token)
-        mPresenter.onMessageArrive = { messages : Array<Message> ->
-            val all = messages.joinToString(
-                    transform = { message -> message.user + message.message },
-                    separator = "\n"
-            )
-            findViewById<TextView>(R.id.textView2).setText(all)
+        mPresenter?.onMessageArrive = { messages : Array<Message> ->
+            mRecyclerView!!.adapter = ChatMessageAdapter(this, messages.toList())
         }
-        mPresenter.getMessages()
+        mPresenter?.getMessages()
         findViewById<Button>(R.id.send_message).setOnClickListener{_ ->
-            mPresenter.sendMessage(findViewById<EditText>(R.id.message_edit_text).text.toString())
+            mPresenter?.sendMessage(findViewById<EditText>(R.id.message_edit_text).text.toString())
             findViewById<EditText>(R.id.message_edit_text).text.clear()
         }
+
+        mRecyclerView = findViewById<RecyclerView>(R.id.chat_recycler_view)
+        //chatRecyclerView.layoutManager = LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false)
+
     }
 
 
--- app/src/main/java/person/hyamada/dummychatclient/chat/ChatMessageAdapter.kt
+++ app/src/main/java/person/hyamada/dummychatclient/chat/ChatMessageAdapter.kt
@@ -1,7 +1,44 @@
 package person.hyamada.dummychatclient.chat
 
-//import android.support.v7.widget.RecyclerView
+import android.content.Context
+import android.support.v7.widget.RecyclerView
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import person.hyamada.dummychatclient.R
+import person.hyamada.dummychatclient.data.Message
 
-class ChatMessageAdapter() {//:RecyclerView.Adapter<ChatMessageAdapter.ViewHolder>() {
+class ChatMessageAdapter(private val context: Context, private val itemList:List<Message>)
+    :RecyclerView.Adapter<ChatMessageAdapter.ChatViewHolder>() {
 
+    private var mRecyclerView : RecyclerView? = null
+
+    override fun onAttachedToRecyclerView(recyclerView: RecyclerView) {
+        super.onAttachedToRecyclerView(recyclerView)
+        mRecyclerView = recyclerView
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ChatViewHolder {
+        val layoutInflater = LayoutInflater.from(context)
+        val mView = layoutInflater.inflate(R.layout.item_chat_message, parent, false)
+
+        return ChatViewHolder(mView)
+    }
+
+    override fun onBindViewHolder(viewHolder: ChatViewHolder, position: Int) {
+        viewHolder?.let {
+            it.messageView.text = itemList.get(position).message
+            it.usernameView.text = itemList.get(position).user
+        }
+    }
+
+    override fun getItemCount(): Int {
+        return itemList.size
+    }
+
+    class ChatViewHolder(view : View): RecyclerView.ViewHolder(view) {
+        val usernameView: TextView = view.findViewById(R.id.username_in_item)
+        val messageView: TextView = view.findViewById(R.id.message_in_item)
+    }
 }
